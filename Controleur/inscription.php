c6af54249258e095b81e21e532a4d419ccc1f953
DELL-PC\DELL <cheikhouna.lo@isep.fr>
2017-05-24 13:18:14 +0200

    modif inscription.php

--- Controleur/inscription.php
+++ Controleur/inscription.php
@@ -8,7 +8,7 @@
 // Connexion à la base de données
 try
 {
-    $dbn = 'mysql:dbname=chome2;host=127.0.0.1';
+    $dbn = 'mysql:dbname=chome;host=127.0.0.1';
 	$bdd = new PDO($dbn, $user_name,  $password );
 
 }
@@ -19,16 +19,10 @@ catch(Exception $e)
 
 // Insertion du message à l'aide d'une requête préparée
 $req = $bdd->prepare('INSERT INTO utilisateur (nom, prenom, mail, mdp, numero_abonne, telephone) VALUES(?, ?, ?, ?, ?, ?)');
-
-//encryptage du mot de passe
-include('../Modele/encryptage.php');
-$TexteCrypte = encrypt($private_key,$_POST['pass']);
-
-$req->execute(array(htmlspecialchars($_POST['nom']),htmlspecialchars($_POST['prenom']),htmlspecialchars($_POST['email']), htmlspecialchars(($TexteCrypte)),htmlspecialchars($_POST['user']),htmlspecialchars($_POST['telephone'])));
-
+$req->execute(array(htmlspecialchars($_POST['nom']),htmlspecialchars($_POST['prenom']),htmlspecialchars($_POST['email']), htmlspecialchars(sha1($_POST['pass'])),htmlspecialchars($_POST['user']),htmlspecialchars($_POST['telephone'])));
 
 echo "Inscription Réussie";
 // Redirection du visiteur vers la page du index.php
-header('Location: ../Vue/index.php');
+header('Location: index.php');
 ?><br/>
 <!--Cliquez <a href="index.php#contac">ici</a> pour retourner à la page d'accueil-->
