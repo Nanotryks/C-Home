235c2f7b349560ca8d72f0fed5b6b3774db917e9
Yonkt <yonkt77@gmail.com>
2017-06-06 09:49:32 +0200

    mise a jour controleur capteur

--- Controleur/Capteur.php
+++ Controleur/Capteur.php
@@ -1,22 +1,76 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Benjamin
- * Date: 16/05/2017
- * Time: 20:06
- */
+
 include '../Modele/Connexion.php';
-$piece=0;
-if(isset($_GET["q"]))
-{
+$piece = 0;
+if (isset($_GET["q"])) {
     $piece = $_GET["q"];
 }
-$reponse = $BDD->query('SELECT Nom FROM capteur WHERE IdPiece="'.$piece.'"');
-$reponse2 = $BDD->query('SELECT Valeur FROM capteur WHERE IdPiece="'.$piece.'"');
+$reponse = $BDD->query('SELECT Nom FROM capteur WHERE IdPiece="' . $piece . '"');
+$reponse2 = $BDD->query('SELECT Valeur FROM capteur WHERE IdPiece="' . $piece . '"');
 
-while($donnees = $reponse->fetch())
-{
+while ($donnees = $reponse->fetch()) {
     $donnees2 = $reponse2->fetch();
 
-    echo $donnees['Nom']." : ".$donnees2['Valeur']."<br>";
+//    echo $donnees['Nom']." : ".$donnees2['Valeur']."<br>";
+
+    if ($donnees['Nom'] == 'Température') {
+        echo $donnees['Nom'] . " : " . $donnees2['Valeur'] . " °C" . "<br>";
+        echo "<br>";
+    } else {
+        if ($donnees['Nom'] == "Humidité") {
+            echo $donnees['Nom'] . " : " . $donnees2['Valeur'] . " %" . "<br>";
+            echo "<br>";
+        } else {
+            if ($donnees['Nom'] == "Lumière") {
+                echo $donnees['Nom'] . " : " . $donnees2['Valeur'] . " Lux" . "<br>";
+                echo "<br>";
+            } else {
+                if ($donnees['Nom'] == "Fumée") {
+                    echo $donnees['Nom'] . " : " . $donnees2['Valeur'] . "<br>";
+                    echo "<br>";
+                } else {
+                    if ($donnees['Nom'] == "Porte d\'entrée") {
+                        if ($donnees2['Valeur'] == 0) {
+                            echo $donnees['Nom'] . " : " . " est fermée" . "<br>";
+                            echo "<br>";
+                        } else if ($donnees2['Valeur'] == 0) {
+                            echo $donnees['Nom'] . " : " . " est ouverte" . "<br>";
+                            echo "<br>";
+                        }
+                    } else {
+
+                        if ($donnees['Nom'] == "Fenêtre") {
+                            if ($donnees2['Valeur'] == 0) {
+                                echo $donnees['Nom'] . " : " . " Fermée" . "<br>";
+                                echo "<br>";
+                            } else if ($donnees2['Valeur'] == 0) {
+                                echo $donnees['Nom'] . " : " . " Ouverte" . "<br>";
+                                echo "<br>";
+                            }
+                        } else {
+                            if ($donnees['Nom'] == "Présence") {
+                                if ($donnees2['Valeur'] == 0) {
+                                    echo $donnees['Nom'] . " : " . "Aucune présence détectée" . "<br>";
+                                    echo "<br>";
+                                } else if ($donnees2['Valeur'] == 0) {
+                                    echo $donnees['Nom'] . " : " . "Présence détectée" . "<br>";
+                                    echo "<br>";
+                                }
+                            } else {
+                                echo "Vous n'avez actuellement aucun capteur de configuré" . "<br>";
+                                echo "<br>";
+                            }
+
+                        }
+
+                    }
+
+
+                }
+            }
+
+        }
+    }
+
+
 }
