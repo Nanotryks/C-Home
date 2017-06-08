3b4fd35192c31951736ac36c8145c25af27717a2
Yonkt <yonkt77@gmail.com>
2017-06-08 09:31:44 +0200

    Modifications de la page Conditions d'utilisation

--- Vue/ConditionUtili.php
+++ Vue/ConditionUtili.php
@@ -7,33 +7,33 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>Condition d'utilisation</title>
-    <link rel="icon" type="image/ico" href="Image/Logopic.ico" />
+    <link rel="icon" type="image/ico" href="Image/Logopic.ico"/>
     <link rel="stylesheet" href="../CSS/ConditionUtili.css"/>
-    <link rel="stylesheet" href="../CSS/design_global.css"/>
+<!--    <link rel="stylesheet" href="../CSS/design_global.css"/>-->
 </head>
 <body>
 
-<div id="ban"> <img src="Image/ban2.png" alt="C-HOME" width="15%" height="auto"/> </div>
+<div id="ban"><img src="../Image/ban.png" alt="C-HOME" width="15%" height="auto"/></div>
 
-<div id= "bloc0">
+<div id="bloc0">
 
     <div id="CondiTitle">
         <h1> Conditions générales d'utilisation </h1>
     </div>
 
 
-    <div id ="cond">
-        
+    <div id="cond">
+
         <br><br>
-    <form action="../Controleur/condgen.php" method="post">
-        <div class="group">
-					<label for="cond" class="label">Entrer les conditions générales d'utilisation</label>
-					<textarea id="cond" name="cond" rows="15" cols="1" type="text" class="input"></textarea>
-        </div>
-    
-        <br><br><br>
-        <input type="submit" class="btn" value="Envoyer">
-    </form>
+        <form action="../Controleur/condgen.php" method="post">
+            <div class="group">
+                <label for="cond" class="label">Entrez les conditions générales d'utilisation :</label>
+                <textarea id="cond" name="cond" rows="15" cols="1" type="text" class="input"></textarea>
+            </div>
+
+            <br><br><br>
+            <input type="submit" class="btn" value="Envoyer">
+        </form>
     </div>
 </div>
 </body>
