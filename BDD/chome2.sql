d72007bce1da85d1cb9cf807d2957e95984c8859
Benjamin <benjamin157@hotmail.fr>
2017-06-10 20:33:09 +0200

    nouvelle base de données

--- BDD/chome2.sql
+++ BDD/chome2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 10 Juin 2017 à 15:38
+-- Généré le :  Sam 10 Juin 2017 à 18:32
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -204,6 +204,8 @@ CREATE TABLE `utilisateur` (
   `mdp` varchar(255) NOT NULL,
   `numero_abonne` varchar(30) NOT NULL,
   `telephone` varchar(30) NOT NULL,
+  `QuestionSecrete` varchar(255) NOT NULL,
+  `ReponseSecrete` varchar(255) NOT NULL,
   `Admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -211,36 +213,40 @@ CREATE TABLE `utilisateur` (
 -- Contenu de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`IdUtilisateur`, `nom`, `prenom`, `mail`, `mdp`, `numero_abonne`, `telephone`, `Admin`) VALUES
-(1, 'jgdgdf', 'dfgsdhf', '123@gmail.com', 'azertyazerty', '1231312', '1231312', 0),
-(2, 'jgdgdf', 'dfgsdhf', '123@gmail.com', 'qsdqsdqsdqsd', '1231312', '1231312', 0),
-(3, 'abcde', 'ddadqss', 'dqsfqfqsfqs@qsdqs', 'sqdqsdq', '1231312', '1231312', 0),
-(4, 'LO', 'qsdqs', '123@gmail.com', 'qsdqsdqs', '1231312', '1231312', 0),
-(5, 'dran', 'gjghhg', 'jdrebd@test', 'sdqshfgjy', '5686797', '6547658', 0),
-(6, 'nouveaunom', 'test2', 'jdrebd@test', 'fhfghfghg', '2222222', '1231312', 0),
-(7, 'dran', 'be', 'benjamin@dran.fr', 'azertyazerty', '999992222', '6943837', 0),
-(8, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '60ca10a2cac0ec59bc65654222b065', '0', '758449357', 0),
-(9, '', '', 'jb', 'a94a8fe5ccb19ba61c4c0873d391e9', '758449357', '758449357', 0),
-(10, 'nom', 'PRENOM', 'jb', '60ca10a2cac0ec59bc65654222b065', '758449357', '12345', 0),
-(11, 'LO', 'CHEIKHOUNA', 'azerty@azerty.com', '52036e5a96b401419e3b870bb38598', '0', '12345', 0),
-(12, 'LO', 'CHEIKHOUNA', 'azerty@azerty.com', '9cf95dacd226dcf43da376cdb6cbba', '12345', '758449357', 0),
-(14, '', '', 'jb', '0b9c2625dc21ef05f6ad4ddf47c5f2', '0', '758449357', 0),
-(15, '', '', 'jb', '0b9c2625dc21ef05f6ad4ddf47c5f2', '123', '758449357', 0),
-(16, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', 'a94a8fe5ccb19ba61c4c0873d391e9', '1994', '758449357', 0),
-(17, '', '', 'jb', '90795a0ffaa8b88c0e250546d8439b', 'tata', '758449357', 0),
-(18, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '90795a0ffaa8b88c0e250546d8439b', 'tata', '758449357', 0),
-(19, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '90795a0ffaa8b88c0e250546d8439b', 'tata', '758449357', 0),
-(20, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '3105221c1c15399d170ef540e974ef4f37f84e93', 'tete', '758449357', 0),
-(21, 'toto', 'tete', 'test@test.com', 'c4033bff94b567a190e33faa551f411caef444f2', '123456789', '1234567', 0),
-(22, 'Chomes', 'G8C', 'G8c@chome.com', 'Chome', '0', '758449357', 0),
-(23, 'Chomes', 'Chomeur', 'Chome@Chome.com', '28df6ce1c4eb2a062b8553511431e54858c4880b', 'Chome', '758449357', 0),
-(24, 'dran', 'be', 'Chome@Chome.com', '8cb2237d0679ca88db6464eac60da96345513964', '12345', '12345', 0),
-(25, 'dell', 'paul', 'pauldell@c-home.com', '8cb2237d0679ca88db6464eac60da96345513964', 'paul', '12345', 0),
-(26, 'LO', 'CHEIKHOUNA', 'azerty@azerty.com', 'a0ff094025db6249d90f911e531633bdaea45616', 'azerty', '123456', 1),
-(27, 'Basile', 'test', 'test@test.com', 'c4033bff94b567a190e33faa551f411caef444f2', 'test', '758449357', 0),
-(28, 'Basile', 'test', 'test@test.com', 'c4033bff94b567a190e33faa551f411caef444f2', 'test', '758449357', 0),
-(29, 'azerty', 'azerty', 'azerty@azerty.fr', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'azerty', '123456789', 0),
-(31, 'qsd', 'qsd', 'qsd@qsd', '81b87be71decdd74ee12a34ad4c278b401bc2a09', 'jb123', '0648624597', 0);
+INSERT INTO `utilisateur` (`IdUtilisateur`, `nom`, `prenom`, `mail`, `mdp`, `numero_abonne`, `telephone`, `QuestionSecrete`, `ReponseSecrete`, `Admin`) VALUES
+(1, 'jgdgdf', 'dfgsdhf', '123@gmail.com', 'azertyazerty', '1231312', '1231312', '', '', 0),
+(2, 'jgdgdf', 'dfgsdhf', '123@gmail.com', 'qsdqsdqsdqsd', '1231312', '1231312', '', '', 0),
+(3, 'abcde', 'ddadqss', 'dqsfqfqsfqs@qsdqs', 'sqdqsdq', '1231312', '1231312', '', '', 0),
+(4, 'LO', 'qsdqs', '123@gmail.com', 'qsdqsdqs', '1231312', '1231312', '', '', 0),
+(5, 'dran', 'gjghhg', 'jdrebd@test', 'sdqshfgjy', '5686797', '6547658', '', '', 0),
+(6, 'nouveaunom', 'test2', 'jdrebd@test', 'fhfghfghg', '2222222', '1231312', '', '', 0),
+(7, 'dran', 'be', 'benjamin@dran.fr', 'azertyazerty', '999992222', '6943837', '', '', 0),
+(8, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '60ca10a2cac0ec59bc65654222b065', '0', '758449357', '', '', 0),
+(9, '', '', 'jb', 'a94a8fe5ccb19ba61c4c0873d391e9', '758449357', '758449357', '', '', 0),
+(10, 'nom', 'PRENOM', 'jb', '60ca10a2cac0ec59bc65654222b065', '758449357', '12345', '', '', 0),
+(11, 'LO', 'CHEIKHOUNA', 'azerty@azerty.com', '52036e5a96b401419e3b870bb38598', '0', '12345', '', '', 0),
+(12, 'LO', 'CHEIKHOUNA', 'azerty@azerty.com', '9cf95dacd226dcf43da376cdb6cbba', '12345', '758449357', '', '', 0),
+(14, '', '', 'jb', '0b9c2625dc21ef05f6ad4ddf47c5f2', '0', '758449357', '', '', 0),
+(15, '', '', 'jb', '0b9c2625dc21ef05f6ad4ddf47c5f2', '123', '758449357', '', '', 0),
+(16, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', 'a94a8fe5ccb19ba61c4c0873d391e9', '1994', '758449357', '', '', 0),
+(17, '', '', 'jb', '90795a0ffaa8b88c0e250546d8439b', 'tata', '758449357', '', '', 0),
+(18, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '90795a0ffaa8b88c0e250546d8439b', 'tata', '758449357', '', '', 0),
+(19, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '90795a0ffaa8b88c0e250546d8439b', 'tata', '758449357', '', '', 0),
+(20, 'LO', 'CHEIKHOUNA', 'cheikhouna@cheikhouna.com', '3105221c1c15399d170ef540e974ef4f37f84e93', 'tete', '758449357', '', '', 0),
+(21, 'toto', 'tete', 'test@test.com', 'c4033bff94b567a190e33faa551f411caef444f2', '123456789', '1234567', '', '', 0),
+(22, 'Chomes', 'G8C', 'G8c@chome.com', 'Chome', '0', '758449357', '', '', 0),
+(23, 'Chomes', 'Chomeur', 'Chome@Chome.com', '28df6ce1c4eb2a062b8553511431e54858c4880b', 'Chome', '758449357', '', '', 0),
+(24, 'dran', 'be', 'Chome@Chome.com', '8cb2237d0679ca88db6464eac60da96345513964', '12345', '12345', '', '', 0),
+(25, 'dell', 'paul', 'pauldell@c-home.com', '8cb2237d0679ca88db6464eac60da96345513964', 'paul', '12345', '', '', 0),
+(26, 'LO', 'CHEIKHOUNA', 'azerty@azerty.com', 'a0ff094025db6249d90f911e531633bdaea45616', 'azerty', '123456', '', '', 1),
+(27, 'Basile', 'test', 'test@test.com', 'c4033bff94b567a190e33faa551f411caef444f2', 'test', '758449357', '', '', 0),
+(28, 'Basile', 'test', 'test@test.com', 'c4033bff94b567a190e33faa551f411caef444f2', 'test', '758449357', '', '', 0),
+(29, 'azerty', 'azerty', 'azerty@azerty.fr', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'azerty', '123456789', '', '', 0),
+(31, 'qsd', 'qsd', 'qsd@qsd', '81b87be71decdd74ee12a34ad4c278b401bc2a09', 'jb123', '0648624597', '', '', 0),
+(32, 'azerty', 'azerty', 'azerty@azerty.fr', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', 'azerty', '0123456789', 'Couleur', 'bleu', 0),
+(33, 'azerty2', 'azerty2', 'azerty@azerty.fr', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'azerty2', '0123456789', 'Couleur', '2dae5f350d5702b3ab80c8e5a9d95393ef66e62e', 0),
+(34, 'azerty3', 'azerty3', 'azerty3@azerty.fr', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'azerty3', '0123456789', 'Couleur', '2dae5f350d5702b3ab80c8e5a9d95393ef66e62e', 0),
+(35, 'ben', 'ben', 'ben@chome.fr', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'ben', '0123456789', 'Couleur', 'd1695e2cec40372ed7283e1e657b7ab18cd3efa9', 0);
 
 --
 -- Index pour les tables exportées
@@ -342,7 +348,7 @@ ALTER TABLE `piece`
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `IdUtilisateur` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `IdUtilisateur` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- Contraintes pour les tables exportées
 --
