944ee1f4a6dfb97cdd14a4fe91c9fdac3120be7c
cheikhounalo <cheikhouna.lo@isep.fr>
2017-06-19 16:12:04 +0200

    création de message.php

---
+++ Controleur/message.php
@@ -0,0 +1,27 @@
+session_start();
+<?php
+   $host_name  = "localhost";
+    $database   = "chome2";
+    $user_name  = "root";
+    $password   = "";
+
+// Connexion à la base de données
+try
+{
+    $dbn = 'mysql:dbname=chome2;host=127.0.0.1';
+	$bdd = new PDO($dbn, $user_name,  $password );
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+// Insertion du message à l'aide d'une requête préparée
+session_start();
+ if (isset($_POST['message'])){
+     $message=htmlspecialchars($_POST['message']);
+ $req = $bdd->prepare('INSERT INTO message (nom, message, contact) VALUES(?, ?, ?)');
+ $req->execute(array($_SESSION['nom'],$message,$_SESSION['telephone']));
+}
